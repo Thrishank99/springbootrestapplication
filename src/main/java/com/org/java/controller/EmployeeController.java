68896146a2b505a9f986e3a93697cce5470129ca
Thrishank99 <thrishank966@gmail.com>
2024-06-28 22:27:48 +0530

    properties added

--- src/main/java/com/org/java/controller/EmployeeController.java
+++ src/main/java/com/org/java/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import java.util.Map.Entry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.org.java.dto.EmployeeDto;
 import com.org.java.entity.Employee;
@@ -28,6 +30,7 @@ import com.org.java.service.EmployeeService;
 @RestController
 @RequestMapping("/employee")
 @SuppressWarnings("unchecked")
+
 public class EmployeeController {
 	Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
--- src/test/java/com/org/java/SpringbootRestApplicationTests.java
+++ src/test/java/com/org/java/SpringbootRestApplicationTests.java
@@ -1,13 +1,122 @@
 package com.org.java;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.event.annotation.AfterTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+import com.org.java.entity.Employee;
+import com.org.java.repository.EmployeeRepository;
+import com.org.java.service.EmployeeService;
+
+
 
 @SpringBootTest
 class SpringbootRestApplicationTests {
 
+	@Autowired
+	private EmployeeService employeeService;
+
+	@MockBean
+	private EmployeeRepository employeeRepository;
+	
+	@BeforeTestClass
+	public void beforeClassTest() {
+		System.out.println("it is print before class will executeded....");	
+	}
+	@Before(value = "caluculateSubTest")
+	public void beforeTest() {
+		System.out.println("will executed before Test");	
+	}
+	
+	
+
+
 	@Test
-	void contextLoads() {
+	public void saveTest() {
+		Employee emp = new Employee(211, "nave", 52, 85, "75");
+		when(employeeRepository.save(emp)).thenReturn(emp);
+		assertEquals(emp, employeeService.saveEmployeeDetails(emp));
+	}
+	@AfterTestClass
+	public void afterTest() {
+		System.out.println("will execute after test");
+		
 	}
 
+	@Test
+	public void updateTest() {
+		  when(employeeRepository.findAll()).thenReturn(Stream .of(new
+				  Employee(102,"Danile", 35.0,55, "Govt"), 
+				  new Employee(102,"Danile", 35.0,55, "Govt"), 
+				  new Employee(102,"Danile", 35.0,55, "Govt")).collect(Collectors.toList()));	
+		  assertEquals(3,employeeService.findAllEmployeeDetails().size());
+		Employee emp = new Employee(102, "prakash", 78.0, 45, "testdept");
+		when(employeeRepository.save(emp)).thenReturn(emp);
+		assertEquals(emp, employeeService.updateEmployeeDetails(emp));
+	}
+
+	@Test
+	public void deleteTest() {
+		when(employeeRepository.findAll()).thenReturn(Stream .of(new
+				  Employee(102,"Danile", 35.0,55, "Govt"), 
+				  new Employee(102,"Danile", 35.0,55, "Govt"), 
+				  new Employee(102,"Danile", 35.0,55, "Govt")).collect(Collectors.toList()));	
+		  assertEquals(3,employeeService.findAllEmployeeDetails().size());
+		Employee emp = new Employee(102,"Danile", 35.0,55, "Govt");
+		employeeService.deleteEmployeeDetails(emp);
+		verify(employeeRepository,times(1)).delete(emp); 
+	} 
+	@Test 
+	  public void findAllEmployeeTest() {
+		  when(employeeRepository.findAll()).thenReturn(Stream .of(new
+		  Employee(100,"Danile", 35.0,55, "Govt"), 
+		  new Employee(100,"Danile", 35.0,55, "Govt"), 
+		  new Employee(100,"Danile", 35.0,55, "Govt")).collect(Collectors.toList()));
+		  assertEquals(3,
+		employeeService.findAllEmployeeDetails().size()); }
+
+	@Test
+	public void findByNameTest() {
+		String Name = "Govt";
+		when(employeeRepository.findByName(Name))
+				.thenReturn(Stream.of(new Employee(101, "Danile", 35, 55500, "Govt")).collect(Collectors.toList()));
+		assertEquals(1, employeeService.findByEmployeNameDeatails(Name).size());
+	}
+	@Test
+	public void findByDepartmentTest() {
+		String Name = "Govt";
+		when(employeeRepository.findByDeptName(Name))
+				.thenReturn(Stream.of(new Employee(101, "Danile", 35, 55500, "Govt")).collect(Collectors.toList()));
+		assertEquals(1, employeeService.findByEmployedeptNameDeatails(Name).size());
+	}
+
+	@Test
+	public void findByIdEmployeeTest() {
+		int employeeId = 1;
+		Employee emp = new Employee(employeeId, "srinu", 5558.0, 505, "Muncipality");
+		doReturn(Optional.of(emp)).when(employeeRepository).findByEmpId(employeeId);
+		Optional<Employee> emp1 = employeeService.findByEmployeeIdDeatails(employeeId);
+		assertNotNull(emp1, "Employee with employeeId : " + employeeId + " not found");
+
+		assertEquals(employeeId, emp1.get().getEmpId());
+		assertEquals(emp.getName(), emp1.get().getName());
+		assertEquals(emp.getSalary(), emp1.get().getSalary());
+		assertEquals(emp.getDepartmentId(), emp1.get().getDepartmentId());
+		assertEquals(emp.getDeptName(), emp1.get().getDeptName());
+	}
 }
